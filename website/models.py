3e9b2fe8e21194d5b4243e2941770c37c752e1cd
Elyash <elyashiv12@gmail.com>
2024-09-22 23:19:28 +0300

    Added gifts attributes: name, link, price and notes; still not support showing the attributes

--- website/models.py
+++ website/models.py
@@ -5,7 +5,10 @@ from sqlalchemy.sql import func
 
 class Gift(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    name = db.Column(db.String(1000))
+    link = db.Column(db.String(10000))
+    price = db.Column(db.Float)
+    notes = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Gift
@@ -10,16 +12,37 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        gift = request.form.get('gift')#Gets the gift from the HTML 
+    if request.method == 'POST':
+        name = request.form.get('gift_name')
+        link = request.form.get('gift_link')
+        price = request.form.get('gift_price')
+        notes = request.form.get('gift_notes')
 
-        if len(gift) < 1:
-            flash('Gift is too short!', category='error') 
-        else:
-            new_gift = Gift(data=gift, user_id=current_user.id)  #providing the schema for the gift 
-            db.session.add(new_gift) #adding the gift to the database 
-            db.session.commit()
-            flash('Gift added!', category='success')
+        if len(name) < 2:
+            flash(f'Invalid gift name. Should be longer then {len(name)} chars', category='error')
+            return render_template("home.html", user=current_user)
+        
+        try:
+            status = requests.get(link, timeout=2000).status_code
+        except requests.exceptions.RequestException as e:
+            flash(f'Invalid link. Details: {e.args}', category='error')
+            return render_template("home.html", user=current_user)
+        if status != 200:
+            flash(f'Gift link is unreachable, status code: {status}', category='error')
+            return render_template("home.html", user=current_user)
+        
+        if price is '':
+            price = -1
+        try:
+            price = float(price)
+        except ValueError as e:
+            flash(f'Invalid price. Details: {e.args}', category='error')
+            return render_template("home.html", user=current_user)
+
+        new_gift = Gift(name=name, link=link, price=price, notes=notes, user_id=current_user.id)  #providing the schema for the gift 
+        db.session.add(new_gift) #adding the gift to the database
+        db.session.commit()
+        flash('Gift added!', category='success')    
 
     return render_template("home.html", user=current_user)
 
